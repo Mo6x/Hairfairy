66d7b0005b5cd13b030c15b842ff2c9f16fdf595
mo6x <christmoses1999@gmail.com>
2025-01-23 22:18:37 +0100

    workinfg on it

--- src/Hooks/queryClient.ts
+++ src/Hooks/queryClient.ts
@@ -1,7 +1,5 @@
 import { QueryClient } from "react-query";
 
-// Create a new QueryClient instance
-const queryClient = new QueryClient();
 
-// Export the QueryClient instance as default
+const queryClient = new QueryClient();
 export default queryClient;
